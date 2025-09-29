ddf38fe97c433b847026c98071500ae0c3fb671d
GK-302 <143247656+GK-302@users.noreply.github.com>
2025-09-29 11:47:13 +0900

    プロジェクト ファイルを追加します。

---
+++ Clippo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Clippo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
